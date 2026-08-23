3258fbd2451cddc1ccbb945903f83ba8605526f6
Ubuntu <ubuntu@localhost.localdomain>
2026-08-23 20:20:57 +0700

    env

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://kasir.onesarumaha.my.id/';
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const api = axios.create({
   baseURL: `${API_BASE_URL}/api`, // Menggunakan variabel API_BASE_URL
