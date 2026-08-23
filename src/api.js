1662a6a2f373807102732d628c5f9f9a2848818a
Ubuntu <ubuntu@localhost.localdomain>
2026-08-23 20:08:48 +0700

    confick

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000';
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://kasir.onesarumaha.my.id/';
 
 const api = axios.create({
   baseURL: `${API_BASE_URL}/api`, // Menggunakan variabel API_BASE_URL
